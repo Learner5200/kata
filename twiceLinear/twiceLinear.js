8486dd0c6d9b14c10e3b246d6efed2a2b8c917e1
Chris Cooksley <christopher.cooksley@gmail.com>
2019-01-10 11:37:41 +0000

    Tests passing

--- twiceLinear/twiceLinear.js
+++ twiceLinear/twiceLinear.js
@@ -1,3 +1,31 @@
-export default class TwiceLinearSolver {
+/*
+Start with 1 in array
+maintain a bench array of the 2x+1 and 3x+1 of every item in the array.
+add the smallest thing in the bench to the list
+add that thing's 2x+1 and 3x+1 to the bench
+iterate until the array is of size
+*/
 
+export default class TwiceLinearSolver {
+  static solve(integer) {
+    const FIRST = 1;
+    const sequence = [FIRST];
+    let numbersToAdd = [this.doubleAddOne(FIRST), this.tripleAddOne(FIRST)];
+    while (sequence.length <= integer) {
+      const nextNumber = Math.min(...numbersToAdd);
+      sequence.push(nextNumber);
+      numbersToAdd = numbersToAdd.filter(number => number !== nextNumber);
+      numbersToAdd.push(this.doubleAddOne(nextNumber));
+      numbersToAdd.push(this.tripleAddOne(nextNumber));
+    }
+    return sequence.pop();
+  }
+
+  static doubleAddOne(integer) {
+    return (2 * integer) + 1;
+  }
+
+  static tripleAddOne(integer) {
+    return (3 * integer) + 1;
+  }
 }
