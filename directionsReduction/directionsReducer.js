59791582920d0bb745d3cb959cc0593be83a6cf5
Chris Cooksley <christopher.cooksley@gmail.com>
2019-01-11 14:02:00 +0000

    Tests passing

--- directionsReduction/directionsReducer.js
+++ directionsReduction/directionsReducer.js
@@ -1,3 +1,26 @@
 export default class DirectionReducer {
+  static reduceOnce(directions) {
+    const oppositeDirections = {
+      NORTH: 'SOUTH',
+      SOUTH: 'NORTH',
+      WEST: 'EAST',
+      EAST: 'WEST',
+    };
+    const reducedDirections = directions.slice();
+    for (let i = 0; i < directions.length; i += 1) {
+      const currentDirection = directions[i];
+      const nextDirection = directions[i + 1];
+      if (oppositeDirections[currentDirection] === nextDirection) {
+        reducedDirections.splice(i, 2);
+      }
+    }
+    return reducedDirections;
+  }
 
+  static reduce(directions) {
+    while (directions.length !== this.reduceOnce(directions).length) {
+      directions = this.reduceOnce(directions);
+    }
+    return directions;
+  }
 }
--- directionsReduction/directionsReducer.test.js
+++ directionsReduction/directionsReducer.test.js
@@ -3,20 +3,20 @@ import DirectionsReducer from './directionsReducer';
 describe('DirectionsReducer', () => {
   describe('.reduce()', () => {
     it('reduces list of adjacent, opposite directions to []', () => {
-      const directions = ['North', 'South'];
-      expect(DirectionsReducer.reduce(directions)).toBe([]);
+      const directions = ['NORTH', 'SOUTH'];
+      expect(DirectionsReducer.reduce(directions)).toEqual([]);
     });
     it('reduces adjacent, opposite directions recursiely', () => {
-      const directions = ['North', 'East', 'West', 'South'];
-      expect(DirectionsReducer.reduce(directions)).toBe([]);
+      const directions = ['NORTH', 'EAST', 'WEST', 'SOUTH'];
+      expect(DirectionsReducer.reduce(directions)).toEqual([]);
     });
     it('does not reduce non-opposite directions', () => {
-      const directions = ['North', 'East'];
-      expect(DirectionsReducer.reduce(directions)).toBe(directions);
+      const directions = ['NORTH', 'EAST'];
+      expect(DirectionsReducer.reduce(directions)).toEqual(directions);
     });
     it('does not reduce non-adjacent opposite directions', () => {
-      const directions = ['North', 'East', 'South', 'West'];
-      expect(DirectionsReducer.reduce(directions)).toBe(directions);
+      const directions = ['NORTH', 'EAST', 'SOUTH', 'WEST'];
+      expect(DirectionsReducer.reduce(directions)).toEqual(directions);
     });
   });
 });
