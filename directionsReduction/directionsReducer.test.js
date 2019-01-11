4a76dea1b717a62c9292a7a68f17dda1bf40f6be
Chris Cooksley <christopher.cooksley@gmail.com>
2019-01-11 12:31:27 +0000

    Failing tests for reducer

---
+++ directionsReduction/directionsReducer.test.js
@@ -0,0 +1,20 @@
+describe('DirectionsReducer', () => {
+  describe('.reduce()', () => {
+    it('reduces list of adjacent, opposite directions to []', () => {
+      const directions = ['North', 'South'];
+      expect(DirectionsReducer.reduce(directions)).toBe([]);
+    });
+    it('reduces adjacent, opposite directions recursiely', () => {
+      const directions = ['North', 'East', 'West', 'South'];
+      expect(DirectionsReducer.reduce(directions)).toBe([]);
+    });
+    it('does not reduce non-opposite directions', () => {
+      const directions = ['North', 'East'];
+      expect(DirectionsReducer.reduce(directions)).toBe(directions);
+    });
+    it('does not reduce non-adjacent opposite directions', () => {
+      const directions = ['North', 'East', 'South', 'West'];
+      expect(DirectionsReducer.reduce(directions)).toBe(directions);
+    });
+  });
+});
